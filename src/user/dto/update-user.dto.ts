9b05e9c6f12f1c570fde493f432ce2486e6e3341
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-12-26 18:15:51 +0300

    `Removed unused validation decorators from UpdateUserDto class`

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,11 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types'
 import { CreateUserDto } from './create-user.dto'
-import { IsOptional } from 'class-validator'
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-    @IsOptional()
-    tgId?: number
-
-    @IsOptional()
-    nickname?: string
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
