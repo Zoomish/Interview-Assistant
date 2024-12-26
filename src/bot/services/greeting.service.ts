dc66e62074155c829bb529ab2a0d81a1cce89bd6
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-12-26 17:56:27 +0300

    `Removed bot's welcome message and user's admin, password, and show modal fields from database schema`

--- src/bot/services/greeting.service.ts
+++ src/bot/services/greeting.service.ts
@@ -10,23 +10,5 @@ export class GreetingService {
             chatId,
             `Привет, ${msg?.chat?.first_name}! Этого бота я делал как дополнение к подарку, так что надеюсь он тебе понравится)`
         )
-        await bot.sendMessage(
-            chatId,
-            `Для того чтобы продолжить зайди пожалуйста в аккаунт)`,
-            {
-                reply_markup: {
-                    keyboard: [
-                        [
-                            {
-                                text: 'Войти в аккаунт',
-                                web_app: {
-                                    url: process.env.URL,
-                                },
-                            },
-                        ],
-                    ],
-                },
-            }
-        )
     }
 }
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -14,9 +14,6 @@ export class User {
     @Column({ nullable: false })
     email: string
 
-    @Column({ default: false, nullable: false })
-    admin: boolean
-
     @Column({ nullable: false })
     name: string
 
@@ -25,12 +22,6 @@ export class User {
 
     @Column({ nullable: true })
     tgId: number
-
-    @Column({ default: false })
-    showModal: boolean
-
-    @Column({ nullable: false })
-    password: string
 
     @CreateDateColumn()
     createdAt: Date
