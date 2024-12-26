b6053aab3b2ab280d804610d5aeb172daf8b116b
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-12-26 20:10:41 +0300

    `Refactor MeService: add getUserProfilePhotos and send photo instead of text`

--- src/bot/services/user/me.service.ts
+++ src/bot/services/user/me.service.ts
@@ -9,14 +9,13 @@ export class MeService {
         const bot: TelegramBot = global.bot
         const msgWait = await bot.sendMessage(msg.chat.id, `Получаю данные...`)
         const user = await this.userService.findOne(msg.chat.id)
+        const photos = await bot.getUserProfilePhotos(user.tgId)
+        console.log(photos)
+
         await bot.deleteMessage(msgWait.chat.id, msgWait.message_id)
-        await bot.getUserProfilePhotos(user.tgId)
-        return await bot.sendMessage(
-            msg.chat.id,
-            `<b>Меня зовут:</b> ${user.name}\n<b>Я админ:</b> ${user.admin ? 'Да' : 'Нет'}`,
-            {
-                parse_mode: 'HTML',
-            }
-        )
+        return await bot.sendPhoto(msg.chat.id, photos.photos[0][0].file_id, {
+            caption: `<b>Меня зовут:</b> ${user.name}\n<b>Я админ:</b> ${user.admin ? 'Да' : 'Нет'}`,
+            parse_mode: 'HTML',
+        })
     }
 }
