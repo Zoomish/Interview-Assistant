31355b8af9b51fac8e0c204e6eff32491ef4a0d9
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-12-26 17:45:27 +0300

    `Added pg package and TypeORM configuration to the project`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,34 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { BotModule } from './bot/bot.module'
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), BotModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get('POSTGRES_HOST'),
+                port: configService.get('POSTGRES_PORT'),
+                username: configService.get('POSTGRES_USER'),
+                password: configService.get('POSTGRES_PASSWORD'),
+                database: configService.get('POSTGRES_DATABASE'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: true,
+                migrationsRun: true,
+                ssl: {
+                    rejectUnauthorized: true,
+                },
+                autoLoadEntities: true,
+            }),
+            inject: [ConfigService],
+        }),
+        BotModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
