382102417d24519f61aefff8ffaef4c5ff6e6007
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-12-26 20:16:03 +0300

    `Refactor MeService: Update getUserProfilePhotos call and photo sending logic`

--- src/bot/services/user/me.service.ts
+++ src/bot/services/user/me.service.ts
@@ -9,13 +9,26 @@ export class MeService {
         const bot: TelegramBot = global.bot
         const msgWait = await bot.sendMessage(msg.chat.id, `Получаю данные...`)
         const user = await this.userService.findOne(msg.chat.id)
-        const photos = await bot.getUserProfilePhotos(user.tgId)
-        console.log(photos)
-
-        await bot.deleteMessage(msgWait.chat.id, msgWait.message_id)
-        return await bot.sendPhoto(msg.chat.id, photos.photos[0][0].file_id, {
-            caption: `<b>Меня зовут:</b> ${user.name}\n<b>Я админ:</b> ${user.admin ? 'Да' : 'Нет'}`,
-            parse_mode: 'HTML',
+        const photos = await bot.getUserProfilePhotos(user.tgId, {
+            offset: 1,
         })
+        await bot.deleteMessage(msgWait.chat.id, msgWait.message_id)
+        if (photos.photos.length) {
+            return await bot.sendPhoto(
+                msg.chat.id,
+                photos.photos[0][0].file_id,
+                {
+                    caption: `<b>Меня зовут:</b> ${user.name}`,
+                    parse_mode: 'HTML',
+                }
+            )
+        }
+        return await bot.sendMessage(
+            msg.chat.id,
+            `<b>Меня зовут:</b> ${user.name}`,
+            {
+                parse_mode: 'HTML',
+            }
+        )
     }
 }
