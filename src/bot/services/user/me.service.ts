9d84d6341cadd0e7c05e33593b9c3896c7b7de87
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-12-26 20:08:27 +0300

    `Added bot.getUserProfilePhotos call and updated return message in MeService`

--- src/bot/services/user/me.service.ts
+++ src/bot/services/user/me.service.ts
@@ -10,6 +10,7 @@ export class MeService {
         const msgWait = await bot.sendMessage(msg.chat.id, `Получаю данные...`)
         const user = await this.userService.findOne(msg.chat.id)
         await bot.deleteMessage(msgWait.chat.id, msgWait.message_id)
+        await bot.getUserProfilePhotos(user.tgId)
         return await bot.sendMessage(
             msg.chat.id,
             `<b>Меня зовут:</b> ${user.name}\n<b>Я админ:</b> ${user.admin ? 'Да' : 'Нет'}`,
