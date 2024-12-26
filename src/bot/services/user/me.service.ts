ad456b896a45c96e19267aa011e3185a67cbb90c
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-12-26 20:21:20 +0300

    `Update MeService to display additional user information in the message`

--- src/bot/services/user/me.service.ts
+++ src/bot/services/user/me.service.ts
@@ -13,7 +13,9 @@ export class MeService {
             offset: 1,
         })
         await bot.deleteMessage(msgWait.chat.id, msgWait.message_id)
-        const text = `<b>Меня зовут:</b> ${user.name}`
+        const text = `<b>Меня зовут:</b> ${user.name}\n<b>Профессия:</b> ${
+            user.profession
+        }\n<b>Уровень:</b> ${user.level}\n<b>Навыки:</b>\n            ${user.skills.join('\n            ')}`
         if (photos.photos.length) {
             return await bot.sendPhoto(
                 msg.chat.id,
