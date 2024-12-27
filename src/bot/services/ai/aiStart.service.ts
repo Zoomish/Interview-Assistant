a16993a5ee9327d88d7f799e118253c060ee72b8
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-12-27 21:17:46 +0300

    `Refactor aiStart.service.ts to use ConfigService and add model initialization`

--- src/bot/services/ai/aiStart.service.ts
+++ src/bot/services/ai/aiStart.service.ts
@@ -1,20 +1,24 @@
-import { GoogleGenerativeAI } from '@google/generative-ai'
+import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai'
 import { Injectable, OnModuleInit } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class AiStartService implements OnModuleInit {
+    constructor(private readonly configService: ConfigService) {}
     async onModuleInit() {
-        const geminiToken = this.configService.get('TELEGRAM_TOKEN')
+        const geminiToken = this.configService.get('GEMINI_API')
+        const genAI = new GoogleGenerativeAI(geminiToken)
+        const model = genAI.getGenerativeModel({
+            model: 'gemini-1.5-flash',
+            systemInstruction:
+                'Ты эйчар, твое имя Максим. Ты технический специалист, который задает технические вопросы по навыкам. Ты отвечаешь, правильно или неправильно человек ответил на вопрос. В случае правильного ответа ты говоришь об этом и задаешь следующий вопрос. В случае неправильного - объясняешь где ошибка и после этого задаешь следующий вопрос',
+        })
+        await this.first(model)
     }
-    private readonly genAI = new GoogleGenerativeAI(process.env.GEMINI_API)
-    private readonly model = this.genAI.getGenerativeModel({
-        model: 'gemini-1.5-flash',
-        systemInstruction:
-            'Ты эйчар, твое имя Максим. Ты технический специалист, который задает технические вопросы по навыкам. Ты отвечаешь, правильно или неправильно человек ответил на вопрос. В случае правильного ответа ты говоришь об этом и задаешь следующий вопрос. В случае неправильного - объясняешь где ошибка и после этого задаешь следующий вопрос',
-    })
-    async first() {
+
+    async first(model: GenerativeModel) {
         const prompt = 'Write a story about a magic backpack.'
-        const result = await this.model.generateContent(prompt)
+        const result = await model.generateContent(prompt)
         const response = result.response
         const text = response.text()
         return text
