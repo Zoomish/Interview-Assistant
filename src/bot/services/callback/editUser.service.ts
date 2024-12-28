d20615b5bcf210dcbbc6685e9c726f2540cc9abe
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-12-28 21:28:50 +0300

    `Refactor editUser service to handle different actions and add Telegram bot functionality`

--- src/bot/services/callback/editUser.service.ts
+++ src/bot/services/callback/editUser.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@nestjs/common'
+import TelegramBot from 'node-telegram-bot-api'
+import { UserInfoService } from '../user'
 
 @Injectable()
 export class EditUserService {
+    constructor(private readonly userInfoService: UserInfoService) {}
+
     async editUser(action, callbackQuery) {
+        const bot: TelegramBot = global.bot
         switch (action) {
-            case 'junior':
-                return await this.editUser('Junior', callbackQuery.id)
-            case 'middle':
-                return await this.editUser('Middle', callbackQuery.id)
-            case 'senior':
-                return await this.editUser('Senior', callbackQuery.id)
+            case 'profession':
+                await bot.answerCallbackQuery(callbackQuery.id, {
+                    text: 'Вы выбрали изменить профессию',
+                })
+                return await this.userInfoService.getProfession()
+            case 'skills':
+                await bot.answerCallbackQuery(callbackQuery.id, {
+                    text: 'Вы выбрали изменить навыки',
+                })
+                return await this.userInfoService.getSkills()
+            case 'level':
+                await bot.answerCallbackQuery(callbackQuery.id, {
+                    text: 'Вы выбрали изменить уровень',
+                })
+                return await this.userInfoService.level()
             default:
                 break
         }
