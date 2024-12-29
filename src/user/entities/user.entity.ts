509a550daf97b118a564b1cfd263143b4f139cdb
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-12-29 18:50:23 +0300

    `Update User entity to use jsonb instead of simple-json for localhistory column`

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -18,7 +18,7 @@ export class User {
     @Column({ nullable: false, default: false })
     admin: boolean
 
-    @Column({ nullable: true, array: true, default: [], type: 'simple-json' })
+    @Column({ nullable: true, array: true, default: [], type: 'jsonb' })
     localhistory: Content[]
 
     @Column({ nullable: false })
