5456dfb01e49c4efc58bbd1f6ce7cf8ae043e727
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-12-30 20:49:32 +0300

    `Update package.json and info.service.ts to fix minor typos and formatting issues.`

--- src/bot/services/handling/info.service.ts
+++ src/bot/services/handling/info.service.ts
@@ -7,7 +7,11 @@ export class InfoService {
         const bot: TelegramBot = global.bot
         await bot.sendMessage(
             chatId,
-            `Этот бот создан мной(@Zoomish) для помощи людям с прохождением собеседований и трудоустройством.\n\n<b>Информация обо мне:</b>\n<b>Меня зовут:</b> <a href='https://t.me/Zoomish'>Резвый Егор</a>\n<b>Мой гитхаб:</b> <a href='https://github.com/Zoomish'>Zoomish</a>\n<b>Мой канал:</b> <a href='https://t.me/ZoomishChannel'>Zoomish Channel</a>\n`,
+            'Этот бот создан мной(@Zoomish) для помощи людям с прохождением собеседований и трудоустройством.\n\n' +
+                '<b>Информация обо мне:</b>\n' +
+                "<b>Меня зовут:</b> <a href='https://t.me/Zoomish'>Резвый Егор</a>\n" +
+                "<b>Мой гитхаб:</b> <a href='https://github.com/Zoomish'>Zoomish</a>\n" +
+                "<b>Мой канал:</b> <a href='https://t.me/ZoomishChannel'>Zoomish Channel</a>\n",
             {
                 parse_mode: 'HTML',
             }
