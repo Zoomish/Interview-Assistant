1fdb53e5315c13d31ddb1658d5d916ecf39157ab
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-12-30 20:54:51 +0300

    `Update InfoService to include bot information and project link`

--- src/bot/services/handling/info.service.ts
+++ src/bot/services/handling/info.service.ts
@@ -9,10 +9,10 @@ export class InfoService {
             chatId,
             'Этот бот создан мной(@Zoomish) для помощи людям с прохождением собеседований и трудоустройством.\n\n' +
                 '<b>Информация обо мне:</b>\n' +
-                "<b>Меня зовут:</b> <a href='https://t.me/Zoomish'>Резвый Егор</a>\n" +
                 "<b>Мой гитхаб:</b> <a href='https://github.com/Zoomish'>Zoomish</a>\n" +
+                "<b>Мой бот:</b> <a href='https://t.me/ZoomishBot'>ZoomishBot</a>\n" +
                 "<b>Мой канал:</b> <a href='https://t.me/ZoomishChannel'>Zoomish Channel</a>\n\n" +
-                'Информация о проекте.\n' +
+                '<b>Информация о проекте:</b>\n' +
                 "<b>Исходный код: </b><a href='https://github.com/Zoomish/Interview-Assistant'>GitHub</a>",
             {
                 parse_mode: 'HTML',
