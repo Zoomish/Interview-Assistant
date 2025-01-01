c55ce761891daf03494d17000985b7aa6509de46
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-01-01 19:47:22 +0300

    `Added Telegram bot instance and callback query handling to EditProfessionService`

--- src/bot/services/callback/user/profession/editProfession.service.ts
+++ src/bot/services/callback/user/profession/editProfession.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common'
+import TelegramBot from 'node-telegram-bot-api'
 import { UserInfoService } from 'src/bot/services/handling'
 
 @Injectable()
 export class EditProfessionService {
     constructor(private readonly userInfoService: UserInfoService) {}
     async start(action: string, id: string) {
+        const bot: TelegramBot = global.bot
         switch (action) {
             case 'start':
+                await bot.answerCallbackQuery(id, {
+                    text: 'Вы выбрали изменить профессию',
+                })
                 return await this.userInfoService.sendProfession()
             case 'end':
                 return await this.userInfoService.sendProfession()
