ca181cb3eba7f74a6bb8668944df7470651fd2e0
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-01-01 19:39:37 +0300

    init

---
+++ src/bot/services/callback/user/profession/editProfession.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@nestjs/common'
+import TelegramBot from 'node-telegram-bot-api'
+import { UserService } from 'src/user/user.service'
+
+@Injectable()
+export class EditProfessionService {
+    constructor(private readonly userService: UserService) {}
+    async editLevel(action, callbackQuery) {
+        switch (action) {
+            case 'intern':
+                return await this.editUser('Intern', callbackQuery.id)
+            case 'junior':
+                return await this.editUser('Junior', callbackQuery.id)
+            case 'middle':
+                return await this.editUser('Middle', callbackQuery.id)
+            case 'senior':
+                return await this.editUser('Senior', callbackQuery.id)
+            case 'lead':
+                return await this.editUser('Lead', callbackQuery.id)
+            default:
+                break
+        }
+    }
+
+    private async editUser(
+        text: 'Intern' | 'Junior' | 'Middle' | 'Senior' | 'Lead',
+        id: string
+    ) {
+        const bot: TelegramBot = global.bot
+        const msg: TelegramBot.Message = global.msg
+        await this.userService.update(msg.chat.id, {
+            level: text,
+            levelExist: true,
+            localhistory: [],
+            startedInterview: false,
+        })
+        await bot.answerCallbackQuery(id, {
+            text: `Вы изменили уровень на ${text}`,
+        })
+        await bot.sendMessage(
+            msg.chat.id,
+            `Данные успешно сохранены, а история очищена!`
+        )
+        return await bot.sendMessage(
+            msg.chat.id,
+            `Спасибо! Теперь можно начинать!`,
+            {
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            {
+                                text: 'Начать собеседование',
+                                callback_data: 'startinterview',
+                            },
+                        ],
+                    ],
+                },
+            }
+        )
+    }
+}
---
+++ src/bot/services/callback/user/profession/index.ts
@@ -0,0 +1,1 @@
+export * from './editProfession.service'
