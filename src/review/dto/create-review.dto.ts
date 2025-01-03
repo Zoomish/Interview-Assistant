17344c3544b6e12a795f96d3fc5c7fe6b018cdaa
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-01-03 18:17:38 +0300

    `Removed optional properties from CreateReviewDto`

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -2,11 +2,5 @@ import { IsNotEmpty } from 'class-validator'
 
 export class CreateReviewDto {
     @IsNotEmpty()
-    text?: string
-
-    @IsNotEmpty()
-    watched?: boolean
-
-    @IsNotEmpty()
-    answer?: string
+    text: string
 }
