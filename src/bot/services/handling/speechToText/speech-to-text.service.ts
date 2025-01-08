47407a4a512b80f9e6b2d3204f40a0880a909325
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-01-08 17:34:00 +0300

    `Update SpeechToTextService to handle audio file processing and add new prompt for model generation.`

--- src/bot/services/handling/speechToText/speech-to-text.service.ts
+++ src/bot/services/handling/speechToText/speech-to-text.service.ts
@@ -46,30 +46,21 @@ export class SpeechToTextService {
             mimeType: 'audio/ogg',
             displayName: 'Audio sample',
         })
-        console.log(uploadResult)
-
         let file = await this.fileManager.getFile(uploadResult.file.name)
         while (file.state === FileState.PROCESSING) {
             process.stdout.write('.')
-            // Sleep for 10 seconds
             await new Promise((resolve) => setTimeout(resolve, 10_000))
-            // Fetch the file from the API again
             file = await this.fileManager.getFile(uploadResult.file.name)
         }
-        console.log(file)
-
         if (file.state === FileState.FAILED) {
             throw new Error('Audio processing failed.')
         }
-
-        // View the response.
         console.log(
             `Uploaded file ${uploadResult.file.displayName} as: ${uploadResult.file.uri}`
         )
-
         try {
             const response = await model.generateContent([
-                'Tell me about this audio clip.',
+                'Скажи что сказал человек в аудио',
                 {
                     fileData: {
                         fileUri: uploadResult.file.uri,
