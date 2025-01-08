1b0a3f0260536a052c4b4a452108635a3a9fe493
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-01-08 18:19:33 +0300

    `Removed user listing and messaging functionality from endAnnouncement method`

--- src/bot/services/handling/announcement/announcement.service.ts
+++ src/bot/services/handling/announcement/announcement.service.ts
@@ -54,43 +54,6 @@ export class GlobalAnnouncementService {
     async endAnnouncement() {
         const bot: TelegramBot = global.bot
         const chatId = global.msg.chat.id
-        const users = await this.userService.findAll()
-        if (users.length === 0) {
-            await bot.sendMessage(chatId, 'Нет ни одного пользователя в базе.')
-            return
-        }
-        await bot.sendMessage(
-            chatId,
-            `<b>Список всех пользователей (${users.length}):</b>`,
-            {
-                parse_mode: 'HTML',
-            }
-        )
-        users.forEach(async (user) => {
-            const messgase =
-                user?.localhistory?.length > 0
-                    ? Math.floor(user.localhistory.length / 2).toString()
-                    : '0'
-            await bot.sendChatAction(chatId, 'typing')
-            await bot.sendMessage(
-                chatId,
-                '<b>Id:</b> ' +
-                    user.tgId.toString() +
-                    '\n<b>Ник:</b> ' +
-                    '@' +
-                    user.nickname +
-                    '\n<b>Сообщения:</b> ' +
-                    messgase +
-                    '\n<b>Имя:</b> ' +
-                    user.name +
-                    '\n<b>Профессия:</b> ' +
-                    user.profession +
-                    '\n<b>Уровень:</b> ' +
-                    user.level,
-                {
-                    parse_mode: 'HTML',
-                }
-            )
-        })
+        await this.userService.update(chatId, {})
     }
 }
