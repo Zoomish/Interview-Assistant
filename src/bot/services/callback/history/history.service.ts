8bba970bdcf5c2ab0a52720948130654883baf0f
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-01-12 19:26:20 +0300

    `Added exports for announcement and history services`

---
+++ src/bot/services/callback/history/history.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common'
+import TelegramBot from 'node-telegram-bot-api'
+import { ReviewService } from 'src/bot/services/handling'
+
+@Injectable()
+export class HistoryCallbackService {
+    constructor(private readonly reviewService: ReviewService) {}
+
+    async editReview(data: string, id: string) {
+        const bot: TelegramBot = global.bot
+        data = data.trim()
+        const action = data.split('-')[0]
+        const reviewId = Number(
+            data.split('-').length > 1 ? data.split('-')[1] : data
+        )
+        switch (action) {
+            case 'start':
+                await bot.answerCallbackQuery(id, {
+                    text: 'Вы выбрали оставить отзыв',
+                })
+                return await this.reviewService.startReview()
+            case 'edit':
+                await bot.answerCallbackQuery(id, {
+                    text: 'Вы выбрали изменить отзыв',
+                })
+                return await this.reviewService.startReview()
+            case 'answer':
+                await bot.answerCallbackQuery(id, {
+                    text: 'Вы выбрали ответить на отзыв',
+                })
+                return await this.reviewService.answerStartReview(reviewId)
+            case 'end':
+                await bot.answerCallbackQuery(id, {
+                    text: 'Вы отменили действие',
+                })
+                return await this.reviewService.endReview()
+            case 'get':
+                await bot.answerCallbackQuery(id, {
+                    text: 'Вы получаете свой отзыв',
+                })
+                return await this.reviewService.getReview()
+            default:
+                break
+        }
+    }
+}
--- src/bot/services/callback/index.ts
+++ src/bot/services/callback/index.ts
@@ -1,6 +1,8 @@
+export * from './announcement'
 export * from './callback.service'
 export * from './getInfo.service'
+export * from './history'
 export * from './interview'
 export * from './review'
 export * from './user'
-export * from './announcement'
+
