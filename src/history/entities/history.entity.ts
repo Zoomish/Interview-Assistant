40725902549123e3890c08738a133cc68995c774
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-01-12 19:22:40 +0300

    `Added createdAt and updatedAt timestamps to History entity`

--- src/history/entities/history.entity.ts
+++ src/history/entities/history.entity.ts
@@ -2,10 +2,12 @@ import { Content } from '@google/generative-ai'
 import { User } from 'src/user/entities/user.entity'
 import {
     Column,
+    CreateDateColumn,
     Entity,
     JoinColumn,
     OneToOne,
     PrimaryGeneratedColumn,
+    UpdateDateColumn,
 } from 'typeorm'
 
 @Entity()
@@ -22,4 +24,10 @@ export class History {
     @OneToOne(() => User, (user) => user.history)
     @JoinColumn({ referencedColumnName: 'tgId' })
     user: User
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
 }
