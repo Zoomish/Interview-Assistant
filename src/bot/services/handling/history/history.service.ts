b368f834645aaba31e138df73f47d107d5d83ee2
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-01-12 18:51:36 +0300

    `Added TelegramBot import and bot.sendMessage call to HistoryService`

--- src/bot/services/handling/history/history.service.ts
+++ src/bot/services/handling/history/history.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common'
+import TelegramBot from 'node-telegram-bot-api'
 import { HistoryGlobalService } from 'src/history/history.service'
 import { UserService } from 'src/user/user.service'
 
@@ -10,8 +11,27 @@ export class HistoryService {
     ) {}
 
     async getGlobalHistory() {
+        const bot: TelegramBot = global.bot
         const chatId = global.msg.chat.id
         const history = await this.historyGlobalService.findOne(chatId)
+        await bot.sendMessage(
+            chatId,
+            '<b>Ваша история:</b>\n' +
+                '<b>Примечание:</b> История обновляется при завершении текущего собеседования',
+            {
+                parse_mode: 'HTML',
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            {
+                                text: 'Очистить историю',
+                                callback_data: 'clearHistory',
+                            },
+                        ],
+                    ],
+                },
+            }
+        )
         console.log(history.globalhistory)
     }
 
