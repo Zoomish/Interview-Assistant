caad29551fb8b7decc2a6415a0a00040a16ed50f
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-04-26 12:50:50 +0300

    `Added UserService dependency and conditional logic to AppController's AAA method`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common'
+import { UserService } from './user/user.service'
 
 @Controller('app')
 export class AppController {
+    constructor(private readonly userService: UserService) {}
     @Get()
     async AAA() {
+        if (new Date().getHours() === 0) {
+            return this.userService.findAll()
+        }
         return {}
     }
 }
